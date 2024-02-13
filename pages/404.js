47e099e16e20524dc6112d16f06070013e4765dd
labidimahdi <labidimahdi99@gmail.com>
2024-02-13 17:40:22 +0100

    initial commit

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+import utilStyles from '../styles/utils.module.css';
+export default function Custom404() {
+    //return <h1>404 - Page Not Found</h1>;
+    return <p className={utilStyles.center}>404 - Page Not Found</p>;
+  }
